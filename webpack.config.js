a7cf6d2922a81bfcaca310c59392853a360ac5f0
semeon <semeon.peradze@gmail.com>
2016-04-12 18:50:36 -0700

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')
+};
+
+module.exports = {
+  // Entry accepts a path or an object of entries. We'll be using the
+  // latter form given it's convenient with more complex configurations.
+  entry: {
+    javascript: PATHS.app + '/app.js',
+    html:       PATHS.app + '/index.html',    
+  },
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel',
+        include: PATHS.app,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      {
+        test: /\.html$/,
+        include: PATHS.app,        
+        loader: "file?name=[name].[ext]",
+      },   
+
+    ]
+  },  
+};
