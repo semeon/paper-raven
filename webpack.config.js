3ab3c2bc5a3ccbefe7714f08444212e63829e9a5
semeon <semeon.peradze@gmail.com>
2016-04-12 20:38:54 -0700

    seems to be working!

--- webpack.config.js
+++ webpack.config.js
@@ -5,21 +5,26 @@ const PATHS = {
   build: path.join(__dirname, 'build')
 };
 
+console.log(PATHS.app);
+
 module.exports = {
   // Entry accepts a path or an object of entries. We'll be using the
   // latter form given it's convenient with more complex configurations.
+
   entry: {
     javascript: PATHS.app + '/app.js',
     html:       PATHS.app + '/index.html',    
   },
+
   output: {
     path: PATHS.build,
-    filename: 'bundle.js'
+    filename: "bundle.js"
   },
+
   module: {
     loaders: [
       {
-        test: /.jsx?$/,
+        test: /.js?$/,
         loader: 'babel',
         include: PATHS.app,
         query: {
@@ -31,7 +36,6 @@ module.exports = {
         include: PATHS.app,        
         loader: "file?name=[name].[ext]",
       },   
-
     ]
   },  
 };
