0f6fe437ebcb4318cd3444e0ad8abcb25b0a7dc2
semeon <semeon.peradze@gmail.com>
2016-04-15 23:50:16 -0700

    gender icon view refactored

--- app/js/views/leftpanel.jsx
+++ app/js/views/leftpanel.jsx
@@ -6,18 +6,17 @@ import Stats from './stats/stats.jsx';
 export class LeftPanel extends React.Component {
   render () {
 
-  	var genderIconNode = <i className="fa fa-male" aria-hidden="true"></i>;
+  	var genderIconClass = "fa fa-male";
   	if (this.props.hero.getGender() == "female") {
-  		genderIconNode = <i className="fa fa-female" aria-hidden="true"></i>;
+  		genderIconClass = "fa fa-female";
   	}
-
 
     return (
 			  <div className="col-xs-6 col-sm-4 col-md-4 col-lg-3">
 
 					<div className="panel panel-default">
 					  <div className="panel-heading">
-					    <h5><strong> {this.props.hero.getName()} {genderIconNode}</strong></h5>
+					    <h5><strong> {this.props.hero.getName()} <i className={genderIconClass} aria-hidden="true"></i></strong></h5>
 					    <p className="panel-title">	HP: 100 | Level: 3 | XP: 2500/3000</p>
 					  </div>
 					  <div className="panel-body">
